525aeb6add848aeaf460c5ffdcb26dca67626715
Ridho <mridho@ruangguru.com>
2024-07-17 16:13:26 +0800

    update style css

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  integrations: [tailwind(), sitemap(), mdx(), pagefind(), svelte()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
