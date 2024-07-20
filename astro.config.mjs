8859f7c08b53675620f4f4ec4728eabb72524907
ridho <ulunmuhammadridho@gmail.com>
2024-07-20 15:43:14 +0800

    remove pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import pagefind from "astro-pagefind";
 
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind(), svelte()],
+  integrations: [tailwind(), sitemap(), mdx(), svelte()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
