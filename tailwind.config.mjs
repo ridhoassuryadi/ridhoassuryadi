89d12a267796e75b82ce4a9601ff0f304d4780c7
ridho <ulunmuhammadridho@gmail.com>
2024-07-20 17:26:44 +0800

    chore(config): disable tailwind config color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: false,
+  darkMode: 'false',
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
